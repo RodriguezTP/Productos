bd04c1a33f7cc28a4a46ab72bf3af9116075452a
srodriguez-2023a@corhuila.edu.co <srodriguez-2023a@corhuila.edu.co>
2025-10-01 18:16:33 -0500

    Se conecta a mySQL

--- src/test/java/com/practica/ApplicationTests.java
+++ src/test/java/com/practica/ApplicationTests.java
@@ -2,9 +2,11 @@ package com.practica;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class ApplicationTests {
+@ActiveProfiles("test")
+class   ApplicationTests {
 
 	@Test
 	void contextLoads() {
